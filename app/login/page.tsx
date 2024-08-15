b977e8dd0fcea5a6a48987f61d77f3986540b18a
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-08-15 19:35:53 +0300

    fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@
 import {loginUser } from "@/lib/actions";
 
 export default function Page() {
-
+ 
   return(
     <div className="h-[calc(100vh-96px)] flex items-center justify-center min-w-[800px]">
 
